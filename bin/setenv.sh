31caef072cf7d2d70f48c21d35cecac75d233e0b
Millihi <mail@milfie.uu.me>
2019-10-04 01:31:42 +0300

    1. Post-cleanup. 2. Fix memory quota exceeded.

--- bin/setenv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export JAVA_OPTS="${JAVA_OPTS} -Xms32m -XX:MaxMetaspaceSize=64m"
-export JAVA_OPTS="${JAVA_OPTS} -XX:+CMSClassUnloadingEnabled -XX:+UseCodeCacheFlushing"
-export JAVA_OPTS="${JAVA_OPTS} -XX:MinHeapFreeRatio=1 -XX:MaxHeapFreeRatio=1"
-export JAVA_OPTS="${JAVA_OPTS} -Dsun.io.useCanonCaches=false -Djava.awt.headless=true"
-export JAVA_OPTS="${JAVA_OPTS} -Djava.net.preferIPv4Stack=true"
-
--- server/bin/setenv.sh
+++ server/bin/setenv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export JAVA_OPTS="${JAVA_OPTS} -Xms32m -XX:MaxMetaspaceSize=64m"
+export JAVA_OPTS="${JAVA_OPTS} -Xms32m -Xmx256m -XX:MaxMetaspaceSize=64m"
 export JAVA_OPTS="${JAVA_OPTS} -XX:+CMSClassUnloadingEnabled -XX:+UseCodeCacheFlushing"
 export JAVA_OPTS="${JAVA_OPTS} -XX:MinHeapFreeRatio=1 -XX:MaxHeapFreeRatio=1"
 export JAVA_OPTS="${JAVA_OPTS} -Dsun.io.useCanonCaches=false -Djava.awt.headless=true"
