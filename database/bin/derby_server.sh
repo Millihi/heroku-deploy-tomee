b40da0f2f584f0db4a55b0303ab99f27b4af8895
Millihi <mail@milfie.uu.me>
2019-10-05 18:55:38 +0300

    Create derby initializer and runner due to bug in original ones.

---
+++ database/bin/derby_server.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+NAME="${NAME:-"${0##*/}"}"
+COMMAND="$1"
+
+if [ ! -d "${DERBY_HOME}" ]; then
+   echo "${NAME}: Error: DERBY_HOME points to a wrong folder."
+   exit 1
+fi
+
+if [ ! -d "${JAVA_HOME}" ]; then
+   echo "${NAME}: Error: JAVA_HOME points to a wrong folder."
+   exit 1
+fi
+
+JAVA_CMD="${JAVA_HOME}/bin/java"
+
+if [ ! -f "${JAVA_CMD}" -o ! -r "${JAVA_CMD}" -o ! -x "${JAVA_CMD}" ]; then
+   echo "${NAME}: Error: Can not see 'java' in [${JAVA_HOME}/bin]."
+   exit 1
+fi
+
+SETENV_LIB="${DERBY_HOME}/bin/setenv.sh"
+
+if [ ! -f "${SETENV_LIB}" -o  ! -r "${SETENV_LIB}" ]; then
+   echo "${NAME}: Error: Can not see 'setenv.sh' in [${DERBY_HOME}/bin]."
+   exit 1
+fi
+
+. "${SETENV_LIB}"
+
+cd "${DERBY_HOME}"
+
+controlCommand=""
+
+case "${COMMAND}" in
+   [Ss][Tt][Aa][Rr][Tt])
+      controlCommand="start"
+      ;;
+   [Ss][Tt][Oo][Pp])
+      controlCommand="shutdown"
+      ;;
+   *)
+      echo "${NAME}: Error: Invalid command '${COMMAND}' given."
+      exit 1
+      ;;
+esac
+
+"${JAVA_CMD}" \
+   "${DERBY_OPTS}" \
+   -classpath "${DERBY_CLASSPATH}" \
+   "org.apache.derby.drda.NetworkServerControl" "${controlCommand}"
--- server/bin/setenv.sh
+++ server/bin/setenv.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-JAVA_OPTS="-Xms32m -Xmx256m -XX:MaxMetaspaceSize=64m"
+JAVA_OPTS=""
+JAVA_OPTS="${JAVA_OPTS} -Xms32m -Xmx256m -XX:MaxMetaspaceSize=64m"
 JAVA_OPTS="${JAVA_OPTS} -XX:+CMSClassUnloadingEnabled"
 JAVA_OPTS="${JAVA_OPTS} -XX:+UseCodeCacheFlushing"
 JAVA_OPTS="${JAVA_OPTS} -XX:MinHeapFreeRatio=1"
@@ -8,5 +9,4 @@ JAVA_OPTS="${JAVA_OPTS} -XX:MaxHeapFreeRatio=1"
 JAVA_OPTS="${JAVA_OPTS} -Dsun.io.useCanonCaches=false"
 JAVA_OPTS="${JAVA_OPTS} -Djava.awt.headless=true"
 JAVA_OPTS="${JAVA_OPTS} -Djava.net.preferIPv4Stack=true"
-JAVA_OPTS="${JAVA_OPTS} -Dhttp.port=${PORT}"
-export JAVA_OPTS
+JAVA_OPTS="${JAVA_OPTS} -Dhttp.port=${PORT:-8080}"
