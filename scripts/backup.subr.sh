8ddef8a54579253ebed60605505b5e1bfbf570fa
Millihi <mail@milfie.uu.me>
2019-10-09 21:14:10 +0300

    Added debug, trigger rebuild.

--- scripts/backup.subr.sh
+++ scripts/backup.subr.sh
@@ -125,7 +125,7 @@ __backup_unlock () {
 __backup_isLocked () {
    local - result
 
-   set +e
+   set +e -x
 
    if [ "$(__backup_getLockState)" ]
    then
@@ -138,7 +138,7 @@ __backup_isLocked () {
 __backup_getLockState () {
    local -
 
-   set +e
+   set +e -x
 
    echo -n "$( \
       curl \
