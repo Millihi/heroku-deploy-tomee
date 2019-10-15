624d25ee6aeca83ea9807173eff23ee8f3aebbda
Millihi <mail@milfie.uu.me>
2019-10-15 07:20:04 +0300

    Fixed version sorting. Improved search algorithm.

--- bin/azul.lib.sh
+++ bin/azul.lib.sh
@@ -1,35 +1,33 @@
 #!/bin/sh
 
+__azul_FS=" "
 __azul_RS="
 "
-__azul_FS=" "
 
 __azul_JDK_REPO_URL="https://cdn.azul.com/zulu/bin"
 __azul_JDK_LIST_URL="https://cdn.azul.com/zulu/bin"
 __azul_JDK_LIST=""
 
 __azul_loadListSource () {
-   local - url
+   local -
 
-   set -e
+   set +e
 
    if [ -z "$1" ]
    then
-      echo "azul.obtainListSource (url) requires parameter"
+      echo "azul.obtainListSource (url) requires parameter" >&2
       exit 255
    fi
 
-   url="$1"; shift
-
-   curl --silent --location "${url}" 2>/dev/null \
-   | grep -o "href=[\"']\?zulu[^>]\{1,\}\.tar\.gz" \
-   | sed -e "s/^href=[\"']\?//"
+   curl --silent --location "$1" 2>/dev/null \
+   | grep -o "href=[\"']\?zulu[^\"'<>[:space:]]\{1,\}" \
+   | sed -e "s/^href=[\"']\?//; s/[\"'<>[:space:]].*$//"
 }
 
 __azul_loadList () {
    local - file line build type jvm arch record
 
-   set -e
+   set +e
 
    while read file
    do
@@ -41,13 +39,7 @@ __azul_loadList () {
       line="${file#zulu}"
       build="${line%%-*}"
       line="${line#*-}"
-
-      if [ "${line%%-*}" != "ca" ]
-      then
-         continue
-      fi
-
-      line="${line#*-}"
+      line="${line#ca-}"
 
       if [ "${line%%-*}" = "fx" -o "${line%%-*}" = "dbg" ]
       then
@@ -62,18 +54,14 @@ __azul_loadList () {
       line="${line#*-}"
       arch="${line%.tar.gz}"
 
-      record="${type}${__azul_FS}"
+      record=""
+      record="${record}${type}${__azul_FS}"
       record="${record}${build}${__azul_FS}"
       record="${record}${jvm}${__azul_FS}"
       record="${record}${arch}${__azul_FS}"
       record="${record}${file}"
 
-      if [ "${__azul_JDK_LIST}" ]
-      then
-         __azul_JDK_LIST="${__azul_JDK_LIST}${__azul_RS}"
-      fi
-
-      __azul_JDK_LIST="${__azul_JDK_LIST}${record}"
+      __azul_JDK_LIST="${__azul_JDK_LIST}${__azul_JDK_LIST:+${__azul_RS}}${record}"
    done \
 <<-EOF
    $(__azul_loadListSource "${__azul_JDK_LIST_URL}")
@@ -81,42 +69,54 @@ EOF
 
    if [ -z "${__azul_JDK_LIST}" ]
    then
-      echo "__azul_loadList (): Unable to load Zulu JVM list"
+      echo "azul.loadList (): Unable to load Zulu JVM list" >&2
       exit 255
    fi
 }
 
 azul_getUrl () {
-   local - type jvm arch _type _build _jvm _arch _file record result
+   local - \
+      type jvm arch jvmP1 jvmP2 \
+      _type _build _jvm _arch _file \
+      record result
 
-   set -e
+   set +e
 
    if [ -z "$1" -o -z "$2" -o -z "$3" ]
    then
-      echo "azul.getUrl (type, jvm, arch) requires parameter"
-      exit 1
+      echo "azul.getUrl (type, jvm, arch) requires parameter" >&2
+      exit 255
    fi
 
    type="$(echo -n "$1" | tr "[:lower:]" "[:upper:]")"; shift
    jvm="$1"; shift
    arch="$1"; shift
 
-   set +e
+   jvmP1="${jvm}"
+   jvmP2="${jvm#1.}"
 
    while read _type _build _jvm _arch _file
    do
-      if [ "${_file}" -a "${_type}" = "${type}" -a "${_arch}" = "${arch}" ]
+      if [ -z "${_file}" ]
       then
-         record="${_jvm}${__azul_FS}"
-         record="${record}${_file}"
-
-         if [ "${result}" ]
-         then
-            result="${result}${__azul_RS}"
-         fi
-
-         result="${result}${record}"
+         continue
       fi
+
+      if [ "${_type}" != "${type}" -o "${_arch}" != "${arch}" ]
+      then
+         continue
+      fi
+
+      if [ "${_jvm#${jvmP1}}" = "${_jvm}" -a "${_jvm#${jvmP2}}" = "${_jvm}" ]
+      then
+         continue
+      fi
+
+      record=""
+      record="${record}${_jvm}${__azul_FS}"
+      record="${record}${_file}"
+
+      result="${result}${result:+${__azul_RS}}${record}"
    done \
 <<-EOF
    ${__azul_JDK_LIST}
@@ -124,12 +124,7 @@ EOF
 
    if [ "${result}" ]
    then
-      result="$(\
-         echo "${result}" \
-         | sed -ne "/^${jvm#1.}/p; /^${jvm}/p" \
-         | sort -brk 1 \
-         | sed -ne "1p" \
-      )"
+      result="$(echo "${result}" | sort -Vbrk 1 | sed -ne "1p")"
    fi
 
    if [ "${result}" ]
@@ -143,19 +138,15 @@ EOF
 azul_getFilename () {
    local - url file
 
-   set -e
+   set +e
 
    if [ -z "$1" ]
    then
-      echo "azul.getFilename (url) requires parameter"
+      echo "azul.getFilename (url) requires parameter" >&2
       exit 255
    fi
 
-   url="$1"; shift
-
-   set +e
-
-   file="${url}"
+   file="$1"
    file="${file##*/}"
    file="${file%.tar.gz}"
 
